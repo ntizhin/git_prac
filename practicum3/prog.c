7ae3b60b08bb3339f249763807e17e6494d57155
ntizhin <nikita_tizhin@mailru>
2021-10-22 22:18:20 +0300

    prog.c valgrind + write

--- practicum3/prog.c
+++ practicum3/prog.c
@@ -9,7 +9,6 @@ struct Tree{
 	struct Tree *left, *right;
 };
 
-
 struct Tree *addTree(struct Tree *T, char *wrd){
 	if(T == NULL){
 		T = calloc(1, sizeof(struct Tree));
@@ -18,18 +17,39 @@ struct Tree *addTree(struct Tree *T, char *wrd){
 		T -> right = NULL;
 		T -> left = NULL;
 	}
-	else if(!strcmp(T -> key, wrd)) T -> count += 1;
+	else if(!strcmp(T -> key, wrd)){	
+		T -> count += 1;
+		free(wrd);
+	}
 	else if(strcmp(T -> key, wrd) > 0) T -> left = addTree(T -> left, wrd);
 	else if(strcmp(T -> key, wrd) < 0) T -> right = addTree(T -> right, wrd);
 	return T;
 }
 
-void TreeWriteF(struct Tree *T, FILE *f, int n){
+int maxTree(struct Tree *T){
 	if(T != NULL){
-		TreeWriteF(T -> left, f, n);
-		fprintf(f, "%s %d %.7f\n", T -> key, T -> count, (float)(T -> count)/n );
-		TreeWriteF(T -> right, f, n);
+		int maxL = maxTree(T -> left), maxR = maxTree(T -> right), maxT = T -> count;
+		if(maxL > maxT) maxT = maxL;
+		if(maxR > maxT) maxT = maxR;  
+		return maxT;
 	}
+	return 0;
+}
+
+void maxTwrite(struct Tree *T, FILE *f, int n, int maxT){
+	if(T != NULL){
+		if(T -> count == maxT){
+			fprintf(f, "%s %d %.7f\n", T -> key, T -> count, (float)(T -> count)/n );
+			T -> count = 0;
+		}
+		maxTwrite(T -> left, f, n, maxT);
+		maxTwrite(T -> right, f, n, maxT);
+	}
+}
+
+void TreeWriteF(struct Tree *T, FILE *f, int n){
+	int maxT;
+	for(; maxT = maxTree(T); maxTwrite(T, f, n, maxT));
 }
 
 int sum(struct Tree *T){
@@ -37,8 +57,20 @@ int sum(struct Tree *T){
 	return T -> count + sum(T -> left) + sum(T -> right);
 }
 
+void delTree(struct Tree *T){
+	if(T != NULL){
+		delTree(T -> left);
+		T -> left = NULL;
+		delTree(T -> right);
+		T -> right = NULL;
+		free(T -> key);
+		free(T);
+	}
+}
+
 int main(int agc, char *argV[]){
 	int i;
+	
 	// INSTALL FILE //
 	FILE *f1, *f2;	
 	f1 = stdin;
@@ -51,9 +83,9 @@ int main(int agc, char *argV[]){
 		printf("Cannot open file.\n");
 		return 0;
 	}	
-	
+
+
 	// General part //
-	
 	int c;
 	struct Tree *T = NULL;
 	const int prt = 20;
@@ -71,8 +103,10 @@ int main(int agc, char *argV[]){
 			}
 			if(len > 0){
 				wrd[len] = 0;
+				wrd = realloc(wrd, (len + 1) * sizeof(char));
 				T = addTree(T, wrd);
 			}
+			else free(wrd);
 			if(ispunct(c)){
 			char *wrd = malloc(2 * sizeof(char));
 			wrd[0] = c;
@@ -83,5 +117,11 @@ int main(int agc, char *argV[]){
 	int n;
 	n = sum(T);
 	TreeWriteF(T, f2, n);
+	
+	delTree(T);
+	T = NULL;
+	
+	fclose(f1);
+	fclose(f2);
 	return 0;
 }
